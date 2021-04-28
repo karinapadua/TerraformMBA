b9843c629b67c4176cf83d26f8a16f7d323962b0
karinapadua <kapadua@gmail.com>
2021-04-27 22:01:28 -0300

    trabalho terraform

---
+++ main.tf
@@ -0,0 +1,179 @@
+# Configure the Azure provider
+terraform {
+    required_version = ">= 0.14.9"
+
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 2.26"
+        }
+    }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true  
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "tf-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "mysql"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_virtual_network.main
+  ]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.example.id
+  depends_on = [
+    azurerm_network_interface.main,
+    azurerm_network_security_group.example
+  ]
+
+}
+
+  resource "azurerm_virtual_machine" "main" {
+    name                  = "tf-vm"
+    location              = azurerm_resource_group.rg.location
+    resource_group_name   = azurerm_resource_group.rg.name
+    network_interface_ids = [azurerm_network_interface.main.id]
+    vm_size               = "Standard_DS1_v2"
+    depends_on = [
+      azurerm_resource_group.rg
+    ]
+
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    turma = "es21"
+  }
+}
+
+resource "null_resource" "apt-mysql-server" {
+  triggers = {
+    order = azurerm_virtual_machine.main.id
+  }
+
+  provisioner "remote-exec" {  
+    connection {
+        type = "ssh"
+        user = "testadmin"
+        password = "Password1234!"
+        host = azurerm_public_ip.example.ip_address
+    }
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y mysql-server"
+    
+    ]
+  }
+}
